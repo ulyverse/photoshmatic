90b318f45805e839032edfafc8d1b4fa9a91a7ae
Ulysses Avellano <ulyssesavellano@gmail.com>
2023-09-15 01:02:55 +0800

    refactored code

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-from utils import PhotoshopFiller
-
-
+from gui import Window
 
 def main():
-    psFiller = PhotoshopFiller(photoshop_path=r"C:\Users\johna\Desktop\Photoshop scripting test\Test 2\practice.psd", 
-                       csv_path=r"C:\Users\johna\Desktop\Photoshop scripting test\Test 2\sample.csv",
-                       json_path=r"C:\Users\johna\Desktop\Photoshamatic\Sizes\upper_jersey.json")
-    
-    psFiller.ps_change_colormode(isRgb=True)
-    psFiller.start()
-
+    Window().run()
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -5,19 +5,21 @@ from photoshop import api as ps
 #built-in modules
 import json
 from pathlib import Path
+from tkinter import ttk
 
 #custom modules
 from sizes import Size
 
 class PhotoshopFiller:
     def start(self):
-        for row in range(len(self.df.index)):
-            name = self.app.activeDocument.name
-            self.app.activeDocument.duplicate(f"{name} - placeholder")
+        log = ""
+        num_rows = len(self.df.index)
+        for row in range(num_rows):
+            name = self._app.activeDocument.name
+            self._app.activeDocument.duplicate(f"{name} - placeholder")
 
             doc_num = row + 1
             path = str(self._psd_path.parent) + f"\{doc_num}"
-
 
             for col in self.df.columns:
                 self._fill_layers(col, self.df.loc[row, col])
@@ -26,10 +28,12 @@ class PhotoshopFiller:
             size_found = self._change_doc_size(cur_row_size_name)
 
             if size_found == False:
-                print(f"#{doc_num} size not found")
+                log += f"#{doc_num} size not found\n"
 
-            self.app.activeDocument.saveAs(path, self._jpg_savepref)
-            self.app.activeDocument.close(ps.SaveOptions.DoNotSaveChanges)
+            self._app.activeDocument.saveAs(path, self._jpg_savepref)
+            self._app.activeDocument.close(ps.SaveOptions.DoNotSaveChanges)
+
+        return log
 
     def print_sizes(self):
         for size in self.sizes:
@@ -42,33 +46,32 @@ class PhotoshopFiller:
         size_found = False
         for size in self.sizes:
             if size.name == size_name:
-                self.app.activeDocument.resizeImage(size.width, size.height, self.app.activeDocument.resolution)
+                self._app.activeDocument.resizeImage(size.width, size.height, self._app.activeDocument.resolution)
                 size_found = True
 
         return size_found
             
 
     def _fill_layers(self, layerName: str, content: str):
-        for layer in self.app.activeDocument.layers:
+        for layer in self._app.activeDocument.layers:
             if layer.name == layerName:
                 layer.textItem.contents = content
         
     def ps_change_colormode(self, isRgb: bool):
-        self.app.activeDocument.changeMode(ps.ChangeMode.ConvertToRGB if isRgb else ps.ChangeMode.ConvertToCMYK)
+        self._app.activeDocument.changeMode(ps.ChangeMode.ConvertToRGB if isRgb else ps.ChangeMode.ConvertToCMYK)
 
-    def __init__(self, photoshop_path:str, csv_path:str, json_path:str) -> None:
-        self._init_photoshop(photoshop_path)
-        self._init_sizes(json_path)
-        self._init_dataframe(csv_path)
+    def __init__(self) -> None:
+        self._RGB_COLORMODE = "sRGB IEC61966-2.1"
+        self._CMYK_COLORMODE = "U.S. Web Coated (SWOP) v2"
 
-    def _init_photoshop(self, ps_path:str):
-        self.app = ps.Application()
+    def init_photoshop(self, ps_path:str):
+        self._app = ps.Application()
         self._psd_path = Path(ps_path)
         self._jpg_savepref = ps.JPEGSaveOptions(quality=12)
-        self.app.preferences.rulerUnits = ps.Units.Inches
-        self.app.open(ps_path)
+        self._app.preferences.rulerUnits = ps.Units.Inches
+        self._app.open(ps_path)
     
-    def _init_sizes(self, json_path: str):
+    def init_sizes(self, json_path: str):
         self.sizes = []
 
         with open(str(json_path)) as s:
@@ -77,8 +80,11 @@ class PhotoshopFiller:
         for json_raw_sizes in json_raw['sizes']:
             self.sizes.append(Size(json_raw_sizes['name'], json_raw_sizes['width'], json_raw_sizes['height']))
 
-    def _init_dataframe(self, csv_path):
+    def init_dataframe(self, csv_path):
         self.df = pd.read_csv(csv_path, dtype={'number':str})
+
+    def get_isRGB(self) -> bool:
+        return True if self._app.activeDocument.colorProfileName == self._RGB_COLORMODE else False
         
         
 
