2ba91c0976370d9c2d6e1e80bfe78a1d9ed11580
Ulysses Avellano <ulyssesavellano@gmail.com>
2023-10-12 18:21:57 +0800

    Added validate version and checked if user has internet connection

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import setup
 from gui import Window
 
+def __dir__():
+    return " "
 
 def main():
-    if setup.setup() or setup.validate_registered_device():
-        Window().run()
-        return
-    
-    while True:
-        if setup.enter_license_code():
+    try:
+        if not setup.validate_version():
+            print("This version of the app isn't supported anymore, please update your app to the latest version.")
+            return
+        
+        if setup.setup() or setup.validate_registered_device():
             Window().run()
-            break
-        else:
-            print("Incorrect Key")
-
+            return
+    
+        while True:
+            if setup.enter_license_code():
+                Window().run()
+                break
+            else:
+                print("Incorrect Key")
+    except Exception as e:
+        print("Cannot access the server, please check your internet connection.")
+        print(e)
+        return
 
 if __name__ == "__main__":
     main()
